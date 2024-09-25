f9e21070c2a06dc07bb854ee0c77180496faef9f
Oliver Nordlander <olivernordlander10@hotmail.com>
2024-09-25 18:12:57 +0200

    added LevelChecker code
    denna kod ska användas till att kolla om spelaren är behörig att åtkomma nästa nivå.

---
+++ Platformer/Assets/Scripts/LevelChecker.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChecker : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox, finishedText, UnfinishedText;
+    [SerializeField] private int requierment;
+    [SerializeField] private int levelToLoad;
+
+    private Animator anim;
+    private bool levelIsLoading = false;
+    private bool levelIsComplete = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //kod som kollar om spelaren har möt kriteriet för att fortsätta
+        if (other.CompareTag("Player"))
+        {
+            if(PlayerMovement.levelsCompleted >= requierment)
+            {
+                //Kod för att spela animationen till dörren
+                anim.SetTrigger("Flag");
+                Invoke("LoadNextLevel", 4.0f);
+                levelIsLoading = true;
+            }
+            else
+            {
+                dialogueBox.SetActive(true);
+                UnfinishedText.SetActive(true);
+            }
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !levelIsLoading)
+        {
+            dialogueBox.SetActive(false);
+            finishedText.SetActive(false);
+            UnfinishedText.SetActive(false);
+        }
+    }
+}
