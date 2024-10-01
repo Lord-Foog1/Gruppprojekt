81079dd51ebcab7f17d8417003bcb71102b351aa
Oliver Nordlander <olivernordlander10@hotmail.com>
2024-10-01 21:24:47 +0200

    small changes to mostly texts

--- Platformer/Assets/Scripts/Trampoline.cs
+++ Platformer/Assets/Scripts/Trampoline.cs
@@ -17,7 +17,7 @@ public class Trampoline : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            audioSource.PlayOneShot(trampoline);
+            audioSource.PlayOneShot(trampoline, 0.3f);
             Rigidbody2D playerRigidbody = other.GetComponent<Rigidbody2D>();
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
             playerRigidbody.AddForce(new Vector2(0, jumpForce));
